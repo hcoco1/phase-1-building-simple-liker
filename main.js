3696ce5466b8a081d14ab255c7d8df00eecec086
Ivan <arias.ivan@gmail.com>
2023-04-11 04:57:19 -0400

    lab done

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+const EMPTY_HEART = '♡'
+const FULL_HEART = '♥'
 document.addEventListener('DOMContentLoaded', () => {
-  // Defining text characters for the empty and full hearts for you to use later.
-  const EMPTY_HEART = '♡'
-  const FULL_HEART = '♥'
   const likeButtons = document.querySelectorAll('.like-glyph');
-  likeButtons.forEach(button => {like(button)});
+  const modal = document.getElementById('modal');
 
-  function like(button) {
+  function displayError(callback) {
+    modal.removeAttribute('class');
+    setTimeout(function(){
+        modal.setAttribute('class','hidden');
+        callback();
+    }, 3000);
+  };
+
+  function likifyMe(button) {
     button.addEventListener('click', (event) => {
       if (event.target.innerHTML === EMPTY_HEART) {
         mimicServerCall().then(() => {
@@ -24,19 +31,15 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   };
 
-  const modal = document.getElementById('modal');
-  function displayError(callback) {
-    modal.removeAttribute('class');
-    setTimeout(function(){
-        modal.setAttribute('class','hidden');
-        callback();
-    }, 3000);
-  };
-
-
-
-
+  likeButtons.forEach(button => {
+    likifyMe(button)
+  });
 });
+
+
+
+
+
 
 
 
