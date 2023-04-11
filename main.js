4dbddb4cb63a05818472b5160d8196c80914b6db
Ivan <arias.ivan@gmail.com>
2023-04-11 04:43:14 -0400

    lab done

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+document.addEventListener('DOMContentLoaded', () => {
+  // Defining text characters for the empty and full hearts for you to use later.
+  const EMPTY_HEART = '♡'
+  const FULL_HEART = '♥'
+  const likeButtons = document.querySelectorAll('.like-glyph');
+  likeButtons.forEach(button => {like(button)});
 
-// Your JavaScript code goes here!
+  function like(button) {
+    button.addEventListener('click', (event) => {
+      if (event.target.innerHTML === EMPTY_HEART) {
+        mimicServerCall().then(() => {
+            event.target.innerHTML = FULL_HEART;
+            event.target.setAttribute('class', 'like-glyph activated-heart');
+        }).catch(() => {
+            displayError(function(){
+                event.target.innerHTML = FULL_HEART;
+                event.target.setAttribute('class', 'like-glyph activated-heart');
+            });
+        });
+      } else {
+        event.target.innerHTML = EMPTY_HEART
+        event.target.setAttribute('class', 'like-glyph')
+      }
+    });
+  };
+
+  const modal = document.getElementById('modal');
+  function displayError(callback) {
+    modal.removeAttribute('class');
+    setTimeout(function(){
+        modal.setAttribute('class','hidden');
+        callback();
+    }, 3000);
+  };
+
+
+
+
+});
+
+
 
 
 
@@ -11,10 +47,10 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
