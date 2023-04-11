dcacddad644e838bef7da57878ad1a654da19400
Ivan <arias.ivan@gmail.com>
2023-04-11 05:16:07 -0400

    lab done

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
 document.addEventListener('DOMContentLoaded', () => {
+  const EMPTY_HEART = '♡'
+  const FULL_HEART = '♥'
   const likeButtons = document.querySelectorAll('.like-glyph');
   const modal = document.getElementById('modal');
 
-  function displayError(callback) {
-    modal.removeAttribute('class');
-    setTimeout(function(){
-        modal.setAttribute('class','hidden');
-        callback();
-    }, 3000);
-  };
+  //likebuttons is an array, we must use forEach
+  likeButtons.forEach(button => {
+    like(button)
+  });
 
-  function likifyMe(button) {
+//When a user clicks
+  function like(button) {
     button.addEventListener('click', (event) => {
       if (event.target.innerHTML === EMPTY_HEART) {
         mimicServerCall().then(() => {
@@ -31,18 +29,15 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   };
 
-  likeButtons.forEach(button => {
-    likifyMe(button)
-  });
+  //Respond to the error
+  function displayError(callback) {
+    modal.removeAttribute('class');
+    setTimeout(function(){
+        modal.setAttribute('class','hidden');
+        callback();
+    }, 3000);
+  };
 });
-
-
-
-
-
-
-
-
 
 
 
